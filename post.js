3fb7c32bbdf27c499e96b67cca3ea16998cf5193
Feyza Yiğiter <feyzayigiter@gmail.com>
2021-09-23 17:13:38 +0300

    post.js added

---
+++ post.js
@@ -0,0 +1,32 @@
+const post1 = "Hello!(1)"
+const post2 = "Hello!(2)"
+const post3 = "Hello!(3)"
+
+const func1 = () => { 
+    console.log("post1") }
+
+const func2 = () => { 
+    console.log("post2") }
+
+const func3 = () => { 
+    console.log("post3") }
+
+    func1();
+    func2();
+    func3();
+
+
+const postNew = "Hello!(new)"
+
+const funcNew = () => { 
+    console.log("postNew") }
+
+    
+    func1();
+    func2();
+    func3();
+    funcNew();
+
+
+
+
